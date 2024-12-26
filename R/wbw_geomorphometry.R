7a8d8d5818392a43b929211826d11e1b115f3d70
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2024-12-26 16:54:09 +1300

    Fixed the way of documenting S7 methods

--- R/wbw_geomorphometry.R
+++ R/wbw_geomorphometry.R
@@ -11,16 +11,13 @@
 #' estimate slope. Given the following clock-type grid cell numbering scheme
 #' (Gallant and Wilson, 2000).
 #'
-#' @usage wbw_slope(x, ..., units = "degrees", z_factor = 1.0)
-#'
-#' @eval rd_input_raster("x")
+#' @eval rd_input_raster("dem")
 #' @param units \code{character},
 #' units of slope: "radians", "degrees", or "percent"
 #' @param z_factor \code{double}, Z conversion factor is only important
 #' when the vertical and horizontal units are not the same in the DEM.
 #' When this is the case, the algorithm will multiply each elevation in the
 #' DEM by the Z conversion factor
-#' @param ... Additional arguments passed to methods.
 #'
 #' @return [WbwRaster] object containing slope values
 #'
@@ -33,10 +30,18 @@
 #' @eval rd_example_geomorph("wbw_slope")
 #'
 #' @export
-wbw_slope <- S7::new_generic("wbw_slope", "x")
+wbw_slope <-
+  S7::new_generic(
+    name = "wbw_slope",
+    dispatch_args = "dem",
+    fun = function(dem, units = "degrees", z_factor = 1.0) {
+      S7::S7_dispatch()
+    }
+  )
+
 
 S7::method(wbw_slope, WbwRaster) <-
-  function(x,
+  function(dem,
            units = "degrees",
            z_factor = 1.0) {
     # Checks
@@ -48,7 +53,7 @@ S7::method(wbw_slope, WbwRaster) <-
     checkmate::assert_double(z_factor)
     # Estimate slope
     out <-
-      wbe$slope(dem = x@source, units = units, z_factor = z_factor)
+      wbe$slope(dem = dem@source, units = units, z_factor = z_factor)
     # Return Raster
     WbwRaster(
       name = paste0("Slope (", units, ")"),
