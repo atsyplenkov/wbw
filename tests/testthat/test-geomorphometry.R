fca292f599f08ee85fdaa3ace5d4c4f0f035ff66
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2024-12-27 11:18:12 +1300

    geomorphometry tests

---
+++ tests/testthat/test-geomorphometry.R
@@ -0,0 +1,33 @@
+library(wbw)
+
+raster_path <-
+  system.file("extdata/dem.tif", package = "wbw")
+x <- wbw_read_raster(raster_path)
+
+test_that(
+  "geomorphometry fails",
+  {
+    # wbw_slope
+    expect_error(wbw_slope(dem = mtcars))
+    expect_error(wbw_slope(x, units = 1))
+    expect_error(wbw_slope(x, units = "dg"))
+    expect_error(wbw_slope(x, z_factor = 2L))
+
+    # wbw_ruggedness_index
+    expect_error(wbw_ruggedness_index(dem = mtcars))
+  }
+)
+
+test_that(
+  "S7 object is returned",
+  {
+    expect_s7_class(
+      wbw_slope(x),
+      WhiteboxRaster
+    )
+    expect_s7_class(
+      wbw_ruggedness_index(x),
+      WhiteboxRaster
+    )
+  }
+)
