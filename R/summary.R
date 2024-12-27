83feed035d52381d8957b1f7af07f7a62552dcb6
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2024-12-27 15:05:53 +1300

    Tests for stdev and variance

--- R/summary.R
+++ R/summary.R
@@ -47,6 +47,47 @@ NULL
   extract_stat(stats, "minimum")
 }
 
+#' Get Standard Deviation value from WhiteboxRaster
+#' @param x WhiteboxRaster object
+#' @param ... additional arguments (not used)
+#' @return numeric value
+#' @export
+stdev <-
+  S7::new_generic(
+    name = "stdev",
+    dispatch_args = "x",
+    fun = function(x) {
+      S7::S7_dispatch()
+    }
+  )
+
+S7::method(stdev, WhiteboxRaster) <-
+  function(x) {
+    check_env(wbe)
+    stats <- wbe$raster_summary_stats(x@source)
+    extract_stat(stats, "standard deviation")
+  }
+
+#' Get Variance value from WhiteboxRaster
+#' @param x WhiteboxRaster object
+#' @param ... additional arguments (not used)
+#' @return numeric value
+#' @export
+variance <-
+  S7::new_generic(
+    name = "variance",
+    dispatch_args = "x",
+    fun = function(x) {
+      S7::S7_dispatch()
+    }
+  )
+
+S7::method(variance, WhiteboxRaster) <-
+  function(x) {
+    check_env(wbe)
+    stats <- wbe$raster_summary_stats(x@source)
+    extract_stat(stats, "variance")
+  }
 
 # Helper function to extract numeric value from a specific stat line
 extract_stat <- function(stats_text, pattern) {
