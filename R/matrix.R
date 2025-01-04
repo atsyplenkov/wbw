29028ae94446fd6cc23c3a6b84e50fba746be9fc
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-01-04 20:57:09 +1300

    feat: as_vector for WhiteboxExtent

--- R/matrix.R
+++ R/matrix.R
@@ -38,43 +38,66 @@ S7::method(as_matrix, WhiteboxRaster) <-
     m
   }
 
-
-#' Convert WhiteboxRaster to Vector
+#' Convert objects to vectors
+#' @name as_vector
 #' @rdname vector
 #' @keywords transform
-#'
+#' 
 #' @description
-#' Converts a WhiteboxRaster object to a vector. The output contains all raster
-#' values in row-major order.
+#' Converts various Whitebox objects to vectors:
+#' * For [WhiteboxRaster]: converts raster values to a vector in row-major order
+#' * For [WhiteboxExtent]: converts extent boundaries to a named vector
 #'
-#' @param raw logical. Should the raw data be returned (`raw = TRUE`) or should
-#'   NoData values be transformed to `NA` (`raw = FALSE`)?
-#' @eval rd_input_raster("x")
+#' @param x Object to convert to vector. Can be:
+#'   * A [WhiteboxRaster] object
+#'   * A [WhiteboxExtent] object
+#' @param raw logical. For [WhiteboxRaster] only: Should the raw data be returned
+#'   (`raw = TRUE`) or should NoData values be transformed to `NA` (`raw = FALSE`)?
+#' 
+#' @return A vector, with type depending on the input:
+#' * For [WhiteboxRaster]: vector containing raster values
+#' * For [WhiteboxExtent]: named vector containing extent values
 #'
-#' @return vector containing raster values
+#' @usage
+#' \S4method{as_vector}{WhiteboxRaster}(x, raw = FALSE)
+#' \S4method{as_vector}{WhiteboxExtent}(x)
 #'
-#' @eval rd_example("as_vector")
+#' @aliases as_vector,WhiteboxRaster-method as_vector,WhiteboxExtent-method
+#'
+#' @examples
+#' f <- system.file("extdata/dem.tif", package = "wbw")
+#' x <- wbw_read_raster(f)
+#' 
+#' # Return WhiteboxRaster's data:
+#' head(as_vector(x))
+#' 
+#' # Return WhiteboxExtent's data:
+#' as_vector(wbw_ext(x))
 #'
 #' @export
-as_vector <-
-  S7::new_generic(
-    name = "as_vector",
-    dispatch_args = "x",
-    fun = function(x, raw = FALSE) {
-      S7::S7_dispatch()
-    }
-  )
+as_vector <- S7::new_generic(
+  name = "as_vector",
+  dispatch_args = "x"
+)
 
-S7::method(as_vector, WhiteboxRaster) <-
-  function(x, raw = FALSE) {
-    checkmate::assert_environment(wbw_env)
-    v <- wbw_env$wbw_to_vector(x@source)
-    v <- as.vector(v)
+S7::method(as_vector, WhiteboxRaster) <- function(x, raw = FALSE) {
+  checkmate::assert_environment(wbw_env)
+  v <- wbw_env$wbw_to_vector(x@source)
+  v <- as.vector(v)
 
-    if (!raw) {
-      wbw_nodata <- x@source$configs$nodata
-      v[v == wbw_nodata] <- NA
-    }
-
-    v
+  if (!raw) {
+    wbw_nodata <- x@source$configs$nodata
+    v[v == wbw_nodata] <- NA
   }
+
+  v
+}
+
+S7::method(as_vector, WhiteboxExtent) <- function(x) {
+  c(
+    "west" = x@west,
+    "east" = x@east,
+    "south" = x@south,
+    "north" = x@north
+  )
+}
