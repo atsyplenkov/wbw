2f99eec4c00a1cddebad2629401f3d35a6d1fcdc
Anatolii Tsyplenkov <atsyplenkov@fastmail.com>
2025-01-09 14:11:14 +1300

    ci: update workflows to match with tinytest infrastructure

--- tests/tinytest/test_zzz.R
+++ tests/tinytest/test_zzz.R
@@ -2,9 +2,3 @@
 version <- wbw_version()
 expect_true(is.character(version))
 expect_true(grepl("^\\d+\\.\\d+\\.\\d+$", version))
-
-# Note: The more complex tests involving mocking from the original testthat version
-# are harder to implement in tinytest. Consider either:
-# 1. Using a different mocking approach
-# 2. Writing simpler tests that don't require mocking
-# 3. Skipping those tests if they're not critical 
